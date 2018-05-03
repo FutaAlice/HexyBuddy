bca11088b4881aebb5974bccfe1e63d2c80cce59
FutaAlice <1611271600@qq.com>
2018-05-03 15:43:24 +0800

    Handle "/rec" GET request

--- httpsrv/main.cpp
+++ httpsrv/main.cpp
@@ -2,28 +2,44 @@
 #include <sstream>
 #include <iostream>
 #include <cstdio>
+#include <map>
+#include <vector>
+#include <tuple>
 
 #include "../hexybuddy/hexybuddy.h"
 #include "../cpp-httplib/httplib.h"
+#include "../json11/json11.hpp"
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 using namespace httplib;
+using namespace json11;
+using namespace hexybuddy;
 
 std::string dump_headers(const Headers& headers)
 {
     std::string s;
     char buf[BUFSIZ];
-
     for (const auto &x : headers)
     {
         snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
         s += buf;
     }
-
     return s;
 }
+
+struct Point
+{
+    tuple<int, int> pos;
+    Point(tuple<int, int> pos) : pos(pos) {}
+    Json to_json() const
+    {
+        return Json::object {
+            {"col", get<0>(pos)}, {"row", get<1>(pos)}
+        };
+    }
+};
 
 int main()
 {
@@ -35,27 +51,38 @@ int main()
         exit(1);
     }
 
-    auto pHandle = new hexybuddy::HexyHandle;
+    HexyHandle handle;
 
     srv.get("/init", [&](const Request &req, Response &res) {
-        bool success = pHandle->init();
+        bool success = handle.init();
         string s = success ? "success" : "fail";
-        res.set_content(s, "text/plain");
+        res.set_content(s, "application/json");
     });
 
     srv.get("/rec", [&](const Request &req, Response &res) {
-        stringstream ss;
-        auto rec = pHandle->getRec();
-        for (auto x : rec)
-        {
-            ss << "col " << get<0>(x) << ", ";
-            ss << "row " << get<1>(x) << endl;
-        }
-        res.set_content(ss.str(), "text/plain");
+        int errCode = 200;
+        string errString = "";
+        vector<Point> points;
+        do {
+            if (!handle.init())
+            {
+                errCode = 400;
+                errString = "Hexy process do not exist! Plz open Hexy before query.";
+                break;
+            }
+            auto rec = handle.getRec();
+            points = { rec.begin(), rec.end() };
+        } while (0);
+        Json obj = Json::object({
+            { "errCode", errCode },
+            { "errString", errString },
+            { "record", points },
+        });
+        res.set_content(obj.dump(), "application/json");
     });
     
     srv.post("/set", [&](const Request &req, Response &res) {
-        pHandle->setPiece(make_tuple(5, 5));
+        handle.setPiece(make_tuple(5, 5));
         cout << dump_headers(req.headers) << endl;
         cout << req.body << endl;
         res.set_content("ok", "text/plain");
