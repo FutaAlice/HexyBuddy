c2e78e60c8673f40e2af6cb5818040aaee7bf2c8
FutaAlice <1611271600@qq.com>
2018-05-03 14:32:18 +0800

    Start a HTTP service.

--- httpsrv/main.cpp
+++ httpsrv/main.cpp
@@ -1,4 +1,7 @@
+#include <string>
+#include <sstream>
 #include <iostream>
+#include <cstdio>
 
 #include "../hexybuddy/hexybuddy.h"
 #include "../cpp-httplib/httplib.h"
@@ -6,16 +9,56 @@
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
+using namespace httplib;
+
+std::string dump_headers(const Headers& headers)
+{
+    std::string s;
+    char buf[BUFSIZ];
+
+    for (const auto &x : headers)
+    {
+        snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
+        s += buf;
+    }
+
+    return s;
+}
 
 int main()
 {
-    auto pHandle = new hexybuddy::HexyHandle;
-    pHandle->init();
-    auto rec = pHandle->getRec();
-    for (auto x : rec)
+    Server srv(HttpVersion::v1_1);
+
+    if (!srv.is_valid())
     {
-        cout << "col " << get<0>(x) << ", ";
-        cout << "row " << get<1>(x) << endl;
+        cout << "server has an error..." << endl;
+        exit(1);
     }
-    pHandle->setPiece(make_tuple(5, 5));
+
+    auto pHandle = new hexybuddy::HexyHandle;
+
+    srv.get("/init", [&](const Request &req, Response &res) {
+        bool success = pHandle->init();
+        string s = success ? "success" : "fail";
+        res.set_content(s, "text/plain");
+    });
+
+    srv.get("/rec", [&](const Request &req, Response &res) {
+        stringstream ss;
+        auto rec = pHandle->getRec();
+        for (auto x : rec)
+        {
+            ss << "col " << get<0>(x) << ", ";
+            ss << "row " << get<1>(x) << endl;
+        }
+        res.set_content(ss.str(), "text/plain");
+    });
+    
+    srv.post("/set", [&](const Request &req, Response &res) {
+        pHandle->setPiece(make_tuple(5, 5));
+        cout << dump_headers(req.headers) << endl;
+        cout << req.body << endl;
+        res.set_content("ok", "text/plain");
+    });
+    srv.listen("localhost", 8080);
 }
