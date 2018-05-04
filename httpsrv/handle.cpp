d0a657fa8279e17fcab04c4dde9dbbb690be04eb
FutaAlice <1611271600@qq.com>
2018-05-04 16:39:47 +0800

    some comment

--- httpsrv/handle.cpp
+++ httpsrv/handle.cpp
@@ -17,8 +17,9 @@ using namespace json11;
 namespace httpsrv {
 namespace handle {
 
-inline namespace inner {
+namespace {
 
+// HexyBuddy 实例
 hexybuddy::HexyHandle handle;
 
 struct Point {
@@ -32,67 +33,90 @@ struct Point {
     }
 };
 
-Json makeResp(int errCode, string errString) {
-    return Json::object {
-        { "errCode", errCode },
-        { "errString", errString },
-    };
-}
+struct ErrorStatus {
+    ErrorStatus(int errCode = 200, string errString = "OK") :
+        code(errCode), str(errString) {
+    }
+    Json to_json() const {
+        return Json::object{
+            { "errCode", code }, { "errString", str }
+        };
+    }
+    int    code;
+    string str;
+};
 
-}
-using namespace inner;
+} // namespace
 
+/**
+ * @brief 判断 HexyBuddy 句柄是否初始化成功
+ * 若初始化成功（即 Hexy 存在于后台进程），返回 200 OK
+ */
 void HexyInit(const Request &req, Response &res) {
-    bool success = handle.init();
-    int errCode = success ? 200 : 400;
-    string errString = success ? "" : "Fail, Hexy process not found.";
+    ErrorStatus err;
+    if (!handle.init()) {
+        err = { 400, "Fail, Hexy process not found." };
+    }
 
-    Json obj = makeResp(errCode, errString);
+    Json obj = Json(err);
     res.set_content(obj.dump(), "application/json");
 }
 
+/**
+ * @brief 获取 Hexy 棋盘上的所有落子记录
+ *  回复 JSON 格式如下：
+ *  {
+ *      "errCode": 200,
+ *      "errString": "OK",
+ *      "records": [
+ *          {"col": 5, "row": 5},
+ *          {"col": 6, "row": 6}
+ *      ]
+ *  }
+ */
 void HexyRec(const Request &req, Response &res)
 {
-    int errCode = 200;
-    string errString = "";
+    ErrorStatus err;
     vector<Point> points;
     do {
         if (!handle.init()) {
-            errCode = 400;
-            errString = "Hexy process do not exist! Plz open Hexy before query.";
+            err = { 400, "Hexy process do not exist! Plz open Hexy before query." };
             break;
         }
         auto rec = handle.getRec();
         points = { rec.begin(), rec.end() };
     } while (0);
-    Json obj = Json::object({
-        { "errCode", errCode },
-        { "errString", errString },
-        { "record", points },
-    });
+
+    Json obj = Json(err);
+    auto items = obj.object_items();
+    items["records"] = Json(points);
+    obj = Json(items);
+
     res.set_content(obj.dump(), "application/json");
 }
 
+/**
+ * @brief 控制 Hexy 落子
+ * 接受 GET/POST 请求
+ * 若成功向 Hexy 程序窗体句柄 SendMessage 则视为成功（无法指定位置在判断落子前是否已存在棋子）
+ */
 void HexySet(const Request &req, Response &res)
 {
-    int errCode = 200;
-    string errString = "";
+    ErrorStatus err;
     do {
         if (!handle.init()) {
-            errCode = 400;
-            errString = "Hexy process do not exist! Plz open Hexy before query.";
+            err = { 400, "Hexy process do not exist! Plz open Hexy before query." };
             break;
         }
         int row, col;
         if ("POST" == req.method) {
-            Json obj = Json::parse(req.body, errString);
+            Json obj = Json::parse(req.body, err.str);
             if (!obj.is_object()) {
-                errCode = 400;
+                err.code = 400;
                 break;
             }
             if (!obj["row"].is_number() || !obj["col"].is_number()) {
-                errCode = 400;
-                errString = R"(Param row/col is required.)";
+                err = { 400, R"(Param row/col is required.)" };
                 break;
             }
             row = obj["row"].int_value();
@@ -103,33 +127,32 @@ void HexySet(const Request &req, Response &res)
             if (req.params.end() == req.params.find("row") ||
                 req.params.end() == req.params.find("col"))
             {
-                errCode = 400;
-                errString = R"(Param row/col is required.)";
+                err = { 400, R"(Param row/col is required.)" };
                 break;
             }
             row = atoi(req.params.find("row")->second.c_str());
             col = atoi(req.params.find("col")->second.c_str());
         }
         if (!handle.setPiece(make_tuple(col, row))) {
-            errCode = 400;
             stringstream ss;
             ss << "Position:(col " << col << ", row " << row << ") illegal not empty!";
-            errString = ss.str();
+            err = { 400, ss.str() };
             break;
         }
     } while (0);
 
-    Json obj = makeResp(errCode, errString);
+    Json obj = Json(err);
     res.set_content(obj.dump(), "application/json");
 }
 
+/**
+ * @brief 消息路由无法匹配等情况的错误处理
+ */
 void Error(const Request& req, Response& res)
 {
-    Json obj = makeResp(res.status, "");
+    Json obj = Json({ res.status, "" });
     res.set_content(obj.dump(), "application/json");
 }
 
 } // namespace httputils
 } // namespace handle
-
-
